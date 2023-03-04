ce06fd62cbf9d1bcfba1ca3a6904774b41d86631
justinpraveen1417 <praveenc@thetopclassentertainment.com>
2023-03-04 16:14:00 +0530

    port-Scanner

---
+++ main.py
@@ -0,0 +1,43 @@
+import click
+import pyfiglet
+import socket
+import sys
+
+#figle for Banner
+Banner = pyfiglet.figlet_format("JUSTIN PRAVEEN")
+print(Banner)
+
+def new (name):
+    return socket.gethostbyname(name)
+a = new(input("Enter hostname :"))
+
+print("...................................................................")
+print("IP is {}".format(a))
+print("...................................................................")
+
+try :
+   
+   for port in range(1,63535):
+    
+    # setup for connections
+    soc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+
+    # connect to the ports
+
+    connection = soc.connect_ex((a,port))
+
+    if connection == 0: print(" Open : {}".format(port))
+    soc.close()
+
+except KeyboardInterrupt :
+   print("Please Try someTimes")
+   sys.exit()
+except socket.gaierror:
+   print("Please Try someTimes")
+   sys.exit()
+except socket.error:
+   print("Please Try someTimes")
+   sys.exit()
+   
+
